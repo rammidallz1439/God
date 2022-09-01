02ed571c31b3c9ca8cb930cb9a5b7112b3118215
Rameshreddy001 <75834162+rammidallz1439@users.noreply.github.com>
2022-09-01 15:57:40 +0530

    changes

--- Assets/Scripts/Firebase/AuthUiManager.cs
+++ Assets/Scripts/Firebase/AuthUiManager.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AuthUiManager : MonoBehaviour
 {
+    public static AuthUiManager instance;
+
+    [Header("references")]
+    [SerializeField]
+    private GameObject checkingForAccountUi;
+    [SerializeField]
+    private GameObject loginUi;
+    [SerializeField]
+    private GameObject registerUi;
+    [SerializeField]
+    private GameObject verifyEmailUi;
+    [SerializeField]
+    private TMP_Text verifyEmailText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance==null)
+        {
+            instance = this;
+        }
+        else if (instance!=null)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void ClearUi()
+    {
+       
+        loginUi.SetActive(false);
+        registerUi.SetActive(false);
+    }
+    void LoginScreen()
+    {
+        ClearUi();
+        loginUi.SetActive(true);
+    }
+    void RegisterScreen()
+    {
+        ClearUi();
+        registerUi.SetActive(true);
     }
 }
--- Assets/Scripts/Firebase/Firebasemanager.cs
+++ Assets/Scripts/Firebase/Firebasemanager.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Firebase;
+using Firebase.Auth;
+using TMPro;
 public class Firebasemanager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static Firebasemanager instance;
+
+    public FirebaseAuth auth;
+    public FirebaseUser user;
+
+    [SerializeField]
+    private TMP_InputField loginEmail;
+    [SerializeField]
+    private TMP_InputField loginPaswrod;
+    [SerializeField]
+    private TMP_Text loginText;
+
+    [SerializeField]
+    private TMP_InputField registerEmail;
+    [SerializeField]
+    private TMP_InputField registerUserName;
+    [SerializeField]
+    private TMP_InputField regiterPassword;
+    [SerializeField]
+    private TMP_InputField registerConfrimPassword;
+    [SerializeField]
+    private TMP_Text registerOutputText;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        DontDestroyOnLoad(gameObject);
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Destroy(instance.gameObject);
+            instance = this;
+        }
     }
 }
