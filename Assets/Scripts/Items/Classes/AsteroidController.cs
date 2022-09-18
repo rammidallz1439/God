96f9d932797968a09cd6151579907b0302131f36
Rameshreddy001 <75834162+rammidallz1439@users.noreply.github.com>
2022-09-18 15:45:21 +0530

    ScriptChanges

---
+++ Assets/Scripts/Items/Classes/AsteroidController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidController : MonoBehaviour
+{
+    public GameObject planet;
+    float minSpeed = 15f;
+    float maxSpped = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        planet = GameObject.FindGameObjectWithTag("Planet");
+        
+      transform.position=  new Vector3(Random.Range(-300,300), Random.Range(-300, 300), Random.Range(-300, 300));
+        transform.localScale = new Vector3(Random.Range(250, 1000),Random.Range(250, 1000), Random.Range(250, 1000));
+    }
+
+    private void Update()
+    {
+        if (PrefabManager.ins.canPool)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, planet.transform.position, Random.Range(minSpeed, maxSpped) * Time.deltaTime);
+
+        }
+
+        if (!PrefabManager.ins.canPool)
+        {
+            StopCoroutine(DelayActive());
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Planet"))
+        {
+            StartCoroutine(DelayActive());
+           
+        }
+    }
+
+    IEnumerator DelayActive()
+    {
+        yield return new WaitForSeconds(3f);
+        gameObject.SetActive(false);
+      
+        PrefabManager.ins.canPool = false;
+    }
+}
--- Assets/Scripts/Items/Classes/PrefabManager.cs
+++ Assets/Scripts/Items/Classes/PrefabManager.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
-public class PrefabManager : MonoBehaviour,IPrefabInstantiater
+
+public class PrefabManager : MonoBehaviour, IPrefabInstantiater
 {
-    [SerializeField]
-    GameObject asteroid;
-
-
-    public GameObject[] asteroidArray;
+    public static PrefabManager ins;
+    public List<GameObject> ObjectsList;
     
+    public GameObject objectToPool;
+
+    GameObject temp;
+
+    [SerializeField]
+    int amountToPool;
+
+    public bool canPool;
+
+    #region
+    void Start()
+    {
+        ObjectsList = new List<GameObject>(); //asteroids list
+        canPool = false;
+        ins = this;
+    }
+
+
+    //function of pooling objects when button is clicked
     public void InstantiatePrefabs()
     {
-        
+
+        canPool = true;
+        if (canPool)
+        {
+            for (int i = 0; i < amountToPool; i++)
+            {
+                temp = Instantiate(objectToPool);
+                temp.SetActive(true);
+                ObjectsList.Add(temp);
+
+            }
+        }
     }
+
+  
+
+    #endregion
 }
+
+
+
+
+
+
