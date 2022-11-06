abbf62eccb3587f1d2a6456a1165ace7550db2e0
Rameshreddy001 <75834162+rammidallz1439@users.noreply.github.com>
2022-11-06 17:07:31 +0530

    new changes

--- Assets/Scripts/PlanetManager/PlanetHealth.cs
+++ Assets/Scripts/PlanetManager/PlanetHealth.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlanetHealth : MonoBehaviour
 {
     public float _health = 100f;
-
     public Material[] _mats;
     Renderer rend;
     private void Start()
@@ -13,16 +12,29 @@ public class PlanetHealth : MonoBehaviour
         rend = GetComponent<Renderer>();
         rend.sharedMaterial = _mats[0]; 
     }
+
+    private void Update()
+    {
+        if (_health <= 0f)
+        {
+            StartCoroutine(ChangeMats());
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Meteor"))
         {
             HealthManager(10f);
-            rend.sharedMaterial = _mats[1];
         }
     }
     public void HealthManager(float damage)
     {
         _health -= damage;
+    }
+
+    IEnumerator ChangeMats()
+    {
+        yield return new WaitForSeconds(1f);
+        rend.sharedMaterial = _mats[1];
     }
 }
